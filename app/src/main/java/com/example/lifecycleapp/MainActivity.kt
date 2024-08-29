b7fd73691346365be4e9441c389ab2f7d41a9ea3
Kolchin13D <kolchin13d@gmail.com>
2024-08-29 14:19:22 +0300

    testing

--- app/src/main/java/com/example/lifecycleapp/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lifecycleapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,49 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        Toast.makeText(this, "onCreate is called",
+            Toast.LENGTH_SHORT).show()
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        Toast.makeText(this, "onStart is called",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(this, "onResume is called",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Toast.makeText(this, "onRestart is called",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(this, "onPause is called",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Toast.makeText(this, "onStop is called",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Toast.makeText(this, "onDestroy is called",
+            Toast.LENGTH_SHORT).show()
     }
 }
