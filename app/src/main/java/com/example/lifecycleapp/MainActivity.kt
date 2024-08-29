b1b593f0e593b4a807d51142eb64a6f46c749746
Kolchin13D <kolchin13d@gmail.com>
2024-08-29 14:35:07 +0300

    testing

--- app/src/main/java/com/example/lifecycleapp/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lifecycleapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,49 +21,70 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        Toast.makeText(this, "onCreate is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onCreate is called",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        val btn: Button = findViewById(R.id.btn)
+        btn.setOnClickListener() {
+            var intent: Intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
 
-        Toast.makeText(this, "onStart is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onStart is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onResume() {
         super.onResume()
 
-        Toast.makeText(this, "onResume is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onResume is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onRestart() {
         super.onRestart()
 
-        Toast.makeText(this, "onRestart is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onRestart is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onPause() {
         super.onPause()
 
-        Toast.makeText(this, "onPause is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onPause is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onStop() {
         super.onStop()
 
-        Toast.makeText(this, "onStop is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onStop is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Toast.makeText(this, "onDestroy is called",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "onDestroy is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
